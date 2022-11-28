ee8ebf9794924c1c366282f41199b86965a0282c
Carlos García <carlos.grh@outlook.com>
2022-11-28 08:37:05 -0500

    create links in layout templates, if not in add page to add an anchor tag using url_for to the add_habits endpoint

--- app.py
+++ app.py
@@ -5,4 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", title="Habit Tracker - Home")
+
+
+
+@app.route("/add-habit", methods=["GET","POST"])
+def add_habit():
+    return render_template("add_habits.html", title="Add Habit")
