f83aad7baf5649a0dd42c2d55bb819d47a1cf1e7
Carlos García <carlos.grh@outlook.com>
2022-11-28 11:59:51 -0500

    create form in index.html which heads to a complete endpoint that ultimately adds to a completions default dictionary(list) this gets from the hidden from the date and habitName, creating the date as a key inside the dictionary and the habit being appended to the list. This is possible because of the defaultdict(list) from the builtin collections library. Added style to the habit form and habit completed result

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
 import datetime
+from collections import defaultdict
 
 # TODO: (End Goal) make a habit tracker for studying languages like Python, Go, Swift and link an api service that
 # shows the weather for that particular date // add function that allows user to select periodicity of habit
@@ -7,6 +8,7 @@ import datetime
 app = Flask(__name__)
 
 habits = ["Test Habits", "Second Habit Test"]
+completions = defaultdict(list)
 
 @app.context_processor
 def add_calc_date_range():
@@ -27,6 +29,7 @@ def home():
     return render_template(
         "index.html",
         habits=habits,title="Habit Tracker - Home", 
+        completions=completions[selected_date],
         selected_date=selected_date
     )
 
@@ -38,3 +41,16 @@ def add_habit():
         habits.append(request.form.get("habit"))
         return redirect(url_for('home'))
     return render_template("add_habits.html", title="Add Habit", selected_date=datetime.date.today())
+
+
+
+
+@app.route("/completed", methods=["POST"])
+def complete():
+    date_string = request.form.get("date")
+    habit = request.form.get("habitName")
+    date = datetime.date.fromisoformat(date_string)
+
+    completions[date].append(habit)
+
+    return redirect(url_for('home', date=date_string))
